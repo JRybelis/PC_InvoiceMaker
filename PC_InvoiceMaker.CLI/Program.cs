44235e48185f44124cda145d2f71e19b5f330ba5
Jokūbas Rybelis <frizjr@protonmail.com>
2023-03-15 12:55:43 +0200

    Setup dependency injection

--- PC_InvoiceMaker.CLI/Program.cs
+++ PC_InvoiceMaker.CLI/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using PC_InvoiceMaker.CLI;
+using PC_InvoiceMaker.CLI.Interfaces;
+using PC_InvoiceMaker.LIB.Interfaces.Services;
+using PC_InvoiceMaker.Logic.Services;
+
+
+class Program
+{
+    public static Task<int> Main(string[] args) =>
+        CommandLineApplication.ExecuteAsync<Program>(args);
+    
+    public async Task<int> OnExcecuteAsync(CancellationToken cancellationToken)
+    {
+        var services = new ServiceCollection();
+
+        services.AddScoped<InvoiceMakerCli>();
+        services.AddScoped<IInvoiceMakerCli>(implementationFactory: service =>
+            service.GetRequiredService<InvoiceMakerCli>());
+        services.AddScoped<IInvoiceViewService>(implementationFactory: service =>
+            service.GetRequiredService<InvoiceMakerCli>());
+        services.AddScoped<IInvoiceService, InvoiceService>();
+
+        await using ServiceProvider serviceProvider = 
+            services.BuildServiceProvider(validateScopes: true);
+
+        using (IServiceScope scope = serviceProvider.CreateScope())
+        {
+            var invoiceMakerConsole = scope.ServiceProvider.GetRequiredService<IInvoiceMakerCli>();
+            invoiceMakerConsole.Run();
+        }
+
+        return Environment.ExitCode;
+    }
+}
